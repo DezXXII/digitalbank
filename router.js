fd0fe5328ca36c82323f6065efb1ca7ec8489400
Matias <mattkramm@hotmail.com>
2022-05-07 00:18:06 -0300

    Worked on main page, still needs responsive design

--- router.js
+++ router.js
@@ -20,22 +20,39 @@ const transaction = require('./controllers/transactionsController');
 // Index
 router.get('/', (req, res) => {
     if(req.session.loggedin) {
-        connection.query(`SELECT firstname, lastname, moneyamount FROM info WHERE infoid = ${req.session.userid}`, function (error, results) {
+        let userDataQuery = `SELECT firstname, lastname, moneyamount FROM info WHERE infoid = ${req.session.userid}`
+        connection.query(userDataQuery, function (error, results) {
             if(error) {
-                console.log(error)
-            } else {
+                console.log(error);
+            }
+            let transactionDataQuery = `SELECT transactionid, senderid, receiverid, firstname, lastname, quantityamount FROM transactions INNER JOIN info ON transactions.receiverid = info.infoid WHERE senderid = ${req.session.userid} OR receiverid = ${req.session.userid}`
+            connection.query(transactionDataQuery, function (error, results2) {
                 res.render('index', {
                     login: true,
                     firstname:results[0].firstname,
                     lastname:results[0].lastname,
-                    money:results[0].moneyamount
-               });
-            }
-        })
+                    money:results[0].moneyamount,
+                    userid:req.session.userid,
+                    results2:results2
+                });
+            });
+        });
     } else {
         res.redirect('/login')
     }
 });
+
+// router.get('/', (req, res) => {
+//     if(req.session.loggedin) {
+//         connection.query(`SELECT firstname AS tfirstname, lastname AS tlastname, quantityamount FROM transactions INNER JOIN info ON transactions.receiverid = info.infoid WHERE senderid = ${req.session.userid} OR receiverid = ${req.session.userid}`, (error, results) => {
+//             if(error) {
+//                 console.log(error)
+//             } else {
+//                 console.log(results[0].tfirstname)
+//             }
+//         })
+//     }
+// })
 
 // -------------------------------------------------------------------------
 
@@ -54,6 +71,12 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/auth', login.authentification);
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+});
 
 // -------------------------------------------------------------------------
 
