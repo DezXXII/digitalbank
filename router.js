e611d8475d41b52c7663d4291bae2e3ecb02265a
Matias <mattkramm@hotmail.com>
2022-05-07 23:48:43 -0300

    Kept designing, worked on some backend fix

--- router.js
+++ router.js
@@ -20,39 +20,34 @@ const transaction = require('./controllers/transactionsController');
 // Index
 router.get('/', (req, res) => {
     if(req.session.loggedin) {
-        let userDataQuery = `SELECT firstname, lastname, moneyamount FROM info WHERE infoid = ${req.session.userid}`
+        const userDataQuery = `SELECT firstname, lastname, moneyamount FROM info WHERE infoid = ${req.session.userid}`
         connection.query(userDataQuery, function (error, results) {
             if(error) {
                 console.log(error);
             }
-            let transactionDataQuery = `SELECT transactionid, senderid, receiverid, firstname, lastname, quantityamount FROM transactions INNER JOIN info ON transactions.receiverid = info.infoid WHERE senderid = ${req.session.userid} OR receiverid = ${req.session.userid}`
+            const transactionDataQuery = `SELECT transactions.transactionid,transactions.senderid,transactions.receiverid,transactions.quantityamount, info.firstname AS sender_firstname, info.lastname AS sender_lastname, userinfo.firstname AS receiver_firstname, userinfo.lastname AS receiver_lastname 
+                                            FROM transactions 
+                                            JOIN info ON transactions.senderid = info.infoid
+                                            JOIN info userinfo ON transactions.receiverid = userinfo.infoid WHERE senderid = ${req.session.userid} OR receiverid = ${req.session.userid}`
             connection.query(transactionDataQuery, function (error, results2) {
-                res.render('index', {
-                    login: true,
-                    firstname:results[0].firstname,
-                    lastname:results[0].lastname,
-                    money:results[0].moneyamount,
-                    userid:req.session.userid,
-                    results2:results2
-                });
+                if(error) {
+                    console.log(error)
+                } else {
+                    res.render('index', {
+                        login: true,
+                        firstname:results[0].firstname,
+                        lastname:results[0].lastname,
+                        money:results[0].moneyamount,
+                        userid:req.session.userid,
+                        results2:results2
+                    });
+                }
             });
         });
     } else {
         res.redirect('/login')
     }
 });
-
-// router.get('/', (req, res) => {
-//     if(req.session.loggedin) {
-//         connection.query(`SELECT firstname AS tfirstname, lastname AS tlastname, quantityamount FROM transactions INNER JOIN info ON transactions.receiverid = info.infoid WHERE senderid = ${req.session.userid} OR receiverid = ${req.session.userid}`, (error, results) => {
-//             if(error) {
-//                 console.log(error)
-//             } else {
-//                 console.log(results[0].tfirstname)
-//             }
-//         })
-//     }
-// })
 
 // -------------------------------------------------------------------------
 
